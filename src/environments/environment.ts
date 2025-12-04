0065b691e547aa9cf0b6df2e08f31a206a679a5f
somiltechivies97 <somil.khalyani@techivies.com>
2025-12-04 18:26:52 +0530

    Updated environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: '',
-  supabaseAnonKey: ''
+  supabaseUrl: 'https://kpzympgabwacrperylcw.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtwenltcGdhYndhY3JwZXJ5bGN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ4MzUwNjksImV4cCI6MjA4MDQxMTA2OX0.dIGKqwd7iNk48XPTl8UdV2IEkZncdUvkzydrcPUqMD4'
 };
